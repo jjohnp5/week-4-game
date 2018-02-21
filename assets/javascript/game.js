9abce986f53a9df849c3f599d435cb0dc20453c7
Lagmay <quickfire@uspicorp.com>
2018-02-21 10:11:37 -0600

    minor refactor. deleted selectionmodel variable due to it being redundant.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ var defaultCharacters = [
         img: "http://ddragon.leagueoflegends.com/cdn/8.3.1/img/champion/Jhin.png",
         attack: 8,
         attackIncrease: 8,
-        cAttack: 12,
+        cAttack: 14,
         health: 150
     },
     {
@@ -14,26 +14,26 @@ var defaultCharacters = [
         img: "http://ddragon.leagueoflegends.com/cdn/8.3.1/img/champion/Ashe.png",
         attack: 10,
         attackIncrease: 10,
-        cAttack: 15,
-        health: 200
+        cAttack: 16,
+        health: 160
     },
     {
         id: 3,
         name: "Aatrox",
         img: "http://ddragon.leagueoflegends.com/cdn/8.3.1/img/champion/Aatrox.png",
-        attack: 12,
-        attackIncrease: 12,
-        cAttack: 17,
-        health: 225
+        attack: 5,
+        attackIncrease: 6,
+        cAttack: 12,
+        health: 165
     },
     {
         id: 4,
         name: "Sona",
         img: "http://ddragon.leagueoflegends.com/cdn/8.3.1/img/champion/Sona.png",
         attack: 8,
-        attackIncrease: 8,
-        cAttack: 15,
-        health: 120
+        attackIncrease: 6,
+        cAttack: 18,
+        health: 145
 
     }
 ]
@@ -41,7 +41,6 @@ var defaultCharacters = [
 var selection = {};
 var defender = {};
 var currentDefenders;
-var selectionModel;
 var characters = [];
 
 start();
@@ -61,8 +60,8 @@ function start(){
         characters.forEach((char, index) => {
             if($(this).attr("alt") == char.name){
                 selection = char;
-                selectionModel = $(`<div class="col-4"><img class="img-responsive char-selected" src="${char.img}" alt="${char.name}" ><h4>${char.health}</h4></div>`);
-                $('.selection').append(selectionModel);
+                
+                $('.selection').append($(`<div class="col-4"><img class="img-responsive char-selected" src="${char.img}" alt="${char.name}" ><h4>${char.health}</h4></div>`));
             }else{
                 currentDefenders.push(char);
             }
@@ -97,23 +96,24 @@ function attackHandler(){
     //check if one of chars die.
     // debugger;
     if(currentDefenders.length == 0){
-        const play = confirm('You killed all defenders! Congratulations! Play Again?')
+        const play = confirm('You killed all defenders! Congratulations! Play Again?') 
         if(play){
             start();
         }
+        $('.attack-button').off();
     }
     if(selection.health <= 0){
         const play = confirm('You died! Game Over. Play again?')
         if(play){
             start();
+            $('.attack-button').off();
         }
+        
     }
     
     if(defender.health <= 0){
         // alert('Defender dead, new defender on the way.');
         $('.current-defender').empty();
-        
-        defender = {};
         renderDefenders();
         handleDefender();
         $('.attack-button').off();
@@ -123,6 +123,8 @@ function attackHandler(){
 
 
 function renderDefenders(){
+    $('.defender-header').text('Defender Selection');
+    $('.defenders').empty();
     currentDefenders.forEach(function(char, index){
 
         $('.defenders').append(`<div class="col-4"><img class="img-responsive char-defenders" src="${char.img}" alt="${char.name}" ><h4>${char.health}</h4></div>`);
@@ -132,6 +134,7 @@ function handleDefender(){
     $('.char-defenders').on("click", function(){
         var currentThis = $(this);
         currentDefenders.forEach((char, index) => {
+            console.log("base this", $(this));
             console.log("outside if", currentThis);
             if(currentThis.attr("alt") == char.name){
                 console.log("inside if", currentThis);
@@ -140,6 +143,7 @@ function handleDefender(){
                 $('.current-defender').append(`<div class="col-4"><img class="img-responsive char-defender" src="${char.img}" alt="${char.name}" ><h4>${char.health}</h4></div>`)
                 currentDefenders.splice(index, 1);
                 $('.attack-button').on('click', attackHandler);
+                $('.defender-header').text('Current Defender');
             }
             
         });
